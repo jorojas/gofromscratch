3d9fdc6436476d8b81d459b0b1657d2f406028f2
Jonathan Rojas <jorojas@novopayment.com>
2024-03-21 19:50:44 -0500

    tablamultiplicar

--- exercises/exercise02.go
+++ exercises/exercise02.go
@@ -2,7 +2,6 @@ package exercises
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -10,33 +9,31 @@ import (
 
 func ShowMultiplicationTable() {
 	fmt.Println("Bienvenido a la tabla de multiplicar. Por favor ingresa un número del 1 al 10 : ")
-	num, err := getNumberFromInput()
-
-	for err != nil {
-		num, err = getNumberFromInput()
-	}
+	num := getNumberFromInput()
 
 	for i := 1; i <= 10; i++ {
 		fmt.Printf("%d x %d = %d \n", num, i, (num * i))
 	}
 }
 
-func getNumberFromInput() (int, error) {
+func getNumberFromInput() int {
 	scanner := bufio.NewScanner(os.Stdin)
 
-	if scanner.Scan() {
-		num, err := strconv.Atoi(scanner.Text())
+	for {
+		if scanner.Scan() {
+			num, err := strconv.Atoi(scanner.Text())
 
-		if err != nil {
-			fmt.Println("El dato ingresado es incorrecto. " + err.Error())
-			return -1, err
-		} else if num > 10 || num < 1 {
-			fmt.Println("El dato ingresado es incorrecto. Debe ser de 1 al 10")
-			return -1, errors.New(fmt.Sprint("Valor inválido ", num))
+			if err != nil {
+				fmt.Println("El dato ingresado es incorrecto. " + err.Error())
+				continue
+			} else if num > 10 || num < 1 {
+				fmt.Println("El dato ingresado es incorrecto. Debe ser de 1 al 10")
+				continue
+			} else {
+				return num
+			}
 		} else {
-			return num, nil
+			panic("Could not open an input scanner")
 		}
-	} else {
-		panic("Could not open an input scanner")
 	}
 }
